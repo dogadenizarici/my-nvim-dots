a18f7073b3951fd371b48bbc27c3f25306e0d9de
Doğa Deniz Arıcı <dogadenizarici@gmail.com>
2024-11-20 12:26:11 +0300

    Add files via upload

---
+++ lua/vim-options.lua
@@ -0,0 +1,9 @@
+local tabspace = 4
+
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=" .. tabspace)
+vim.cmd("set softtabstop=" .. tabspace)
+vim.cmd("set shiftwidth=" .. tabspace)
+vim.g.mapleader = " "
